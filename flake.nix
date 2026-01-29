044630bf4ad8db4e380cf75dcc37bc746d99e1f8
philxws692 <63463378+philxws692@users.noreply.github.com>
2026-01-29 10:47:50 +0100

    feat(flake): add nix developer shell (#3)
    * feat(flake): add nix developer shell
    
    * feat(flake): add direnv support for flake
    
    * feat(flake): added helm to nix flake
    
    ---------
    
    Co-authored-by: niri81 <38499069+niri81@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Dev Shell for development of guggr";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              gopls
+              golangci-lint
+              just
+              pnpm
+              nodejs_24
+              buf
+              trufflehog
+              prek
+              kubernetes-helm
+            ];
+          };
+        }
+      );
+    };
+}
