8ceb53792edb461ae891732b880342c9902a74d9
philxws692 <63463378+philxws692@users.noreply.github.com>
2026-01-29 15:29:37 +0100

    feat(agent): parallel jobs
    this ensures that the queue is completely emptied per poll

--- agent/internal/adapters/inbound/rabbitmq/adapter.go
+++ agent/internal/adapters/inbound/rabbitmq/adapter.go
@@ -31,29 +31,37 @@ func NewRabbitMQAdapter(conn *amqp.Connection, queueName string, service service
 	}, nil
 }
 
-func (w *RabbitMQAdapter) Start(interval time.Duration) {
+func (a *RabbitMQAdapter) Start(interval time.Duration) {
 	ticker := time.NewTicker(interval)
 	for range ticker.C {
-		// Get message from queue
-		msg, ok, _ := w.channel.Get(w.queue, true)
-		if !ok {
-			// No message in Queue
-			continue
-		}
+		for {
+			// Get message from queue
+			msg, ok, err := a.channel.Get(a.queue, true)
+			if err != nil {
+				slog.Error("error getting messages from rabbitmq", "error", err)
+			}
+			if !ok {
+				// No message in Queue
+				break
+			}
 
-		// Unmarshal protobuf
-		var j job.Job
-		if err := proto.Unmarshal(msg.Body, &j); err != nil {
-			slog.Error("error decoding job from rabbitmq", "error", err)
-			continue
-		}
+			// create a go routine for every job
+			go func(d []byte) {
+				// Unmarshal protobuf
+				var j job.Job
+				if err := proto.Unmarshal(msg.Body, &j); err != nil {
+					slog.Error("error decoding job from rabbitmq", "error", err)
+					return
+				}
 
-		// Send job to core
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		err := w.service.ProcessJob(ctx, &j)
-		if err != nil {
-			slog.Error("error executing job", "jobid", j.GetId(), "error", err)
+				// Send job to core
+				ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+				defer cancel()
+
+				if err := a.service.ProcessJob(ctx, &j); err != nil {
+					slog.Error("error executing job", "jobid", j.GetId(), "error", err)
+				}
+			}(msg.Body)
 		}
-		cancel()
 	}
 }
