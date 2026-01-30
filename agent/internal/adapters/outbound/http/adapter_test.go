a7b1553f444184d95fcf5133c84027ce3d4964ee
philxws692 <63463378+philxws692@users.noreply.github.com>
2026-01-30 10:39:38 +0100

    test(agent): http adapter testing
    fix(agent): add real id for http testing case

---
+++ agent/internal/adapters/outbound/http/adapter_test.go
@@ -0,0 +1,50 @@
+package http
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/MarvinJWendt/testza"
+	job "github.com/guggr/guggr/gen/proto/go/job"
+	types "github.com/guggr/guggr/gen/proto/go/job/types"
+	jobresult "github.com/guggr/guggr/gen/proto/go/result"
+	resulttypes "github.com/guggr/guggr/gen/proto/go/result/types"
+	"github.com/jarcoal/httpmock"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func TestExecute(t *testing.T) {
+	httpmock.Activate(t)
+
+	httpmock.RegisterResponder("GET", "http://guggr.example", httpmock.NewStringResponder(200, "lorem ipsum"))
+
+	sampleJob := job.Job{
+		Id: "vNRUTtYc-RpMu6JAI2TMJ",
+		Http: &types.HttpJobType{
+			Url: "http://guggr.example",
+		},
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := NewAdapter().Execute(ctx, &sampleJob)
+	if err != nil {
+		t.Errorf("error testing http execute: %v", err)
+	}
+
+	wantedResult := jobresult.JobResult{
+		Id: sampleJob.GetId(),
+		Timestamp: &timestamppb.Timestamp{
+			Seconds: timestamppb.Now().GetSeconds(),
+		},
+		Http: &resulttypes.HttpJobResult{
+			Reachable:  true,
+			IpAddress:  []byte(sampleJob.GetHttp().GetUrl()),
+			StatusCode: 200,
+			Payload:    []byte("lorem ipsum"),
+		},
+	}
+
+	testza.AssertEqualValues(t, result, wantedResult)
+}
