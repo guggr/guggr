84969fa1de48703dff6f721a7e699fda3fba96a4
philxws692 <63463378+philxws692@users.noreply.github.com>
2026-02-05 14:44:39 +0100

    feat(protobuf): add generated protobuf for Rust (#24)
    * chore(.gitignore): add target folder to .gitignore
    
    * feat(protobuf): add generated proto for rust
    
    * refactor(protobuf): remove unused validations
    
    * fix(protobuf): remove guggr-agent from workspace
    
    * style(protobuf): formatted build.rs
    
    * refactor(protobuf): moved rust generated files
    
    moved from crates/guggr-protobuf to gen/proto_types
    
    * refactor(protobuf): changed job type to enum
    
    this allows for pattern matching later on to determine which job type is
    provided
    
    * fix(protobuf): introduce job details again
    
    * ci: rust additions (#25)
    
    * ci: add cargo nextest
    
    * ci: add rusty pre commit hooks
    
    * ci(dependabot): add rust ecosystem
    
    * ci(actions): add rusty linting & formatting checks
    
    * ci(actions): move pr labeling actions to seperate file
    
    * ci(actions): fix small syntax error
    
    * ci(actions): fix nightly & nextest issue
    
    * ci(actions): fix nextest issue
    
    * ci(actions): fix wrong action version
    
    * ci(actions): fix missing protc binary
    
    * ci(actions): fix missing permissions for installing protoc
    
    * ci(actions): remove unnecessary build
    
    * ci: add more pre-commit-hooks and fix issues they found
    
    * ci: add new flake dependencies
    
    * fix: resolve requested change
    
    * ci: add caching for rust
    
    * ci: move cargo nextest install before cache enabling
    
    * ci: move cargo nextest to its own job
    
    * ci: add caching for apt packages
    
    ---------
    
    Co-authored-by: melo <58705527+melo-afk@users.noreply.github.com>

---
+++ gen/proto_types/build.rs
@@ -0,0 +1,45 @@
+use std::{fs, path::PathBuf};
+
+use protocheck_build::compile_protos_with_validators;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=../../proto/");
+    let mut config = prost_build::Config::default();
+
+    config.out_dir("src");
+
+    // Create output directory
+    fs::create_dir_all("src").expect("could not create `src` folder");
+
+    let root = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let proto_dir = root.join("../../proto");
+
+    // Proto files
+    let proto_job = proto_dir.join("job").join("job.proto");
+    let proto_http_job_type = proto_dir.join("job").join("types").join("http.proto");
+    let proto_ping_job_type = proto_dir.join("job").join("types").join("ping.proto");
+
+    let proto_result = proto_dir.join("result").join("result.proto");
+    let proto_http_result_type = proto_dir.join("result").join("types").join("http.proto");
+    let proto_ping_result_type = proto_dir.join("result").join("types").join("ping.proto");
+
+    let proto_files = &[
+        proto_job,
+        proto_http_job_type,
+        proto_ping_job_type,
+        proto_result,
+        proto_http_result_type,
+        proto_ping_result_type,
+    ];
+
+    compile_protos_with_validators(
+        &mut config,
+        &proto_files.clone(),
+        &[&proto_dir],
+        &["guggr.v1"],
+    )?;
+
+    config.compile_protos(proto_files, &[proto_dir])?;
+
+    Ok(())
+}
---
+++ gen/proto_types/src/lib.rs
@@ -0,0 +1,24 @@
+extern crate protocheck;
+
+pub mod job {
+    pub mod v1 {
+        include!("guggr.job.v1.rs");
+    }
+    pub mod types {
+        pub mod v1 {
+            include!("guggr.job.types.v1.rs");
+        }
+    }
+}
+
+pub mod job_result {
+    pub mod v1 {
+        include!("guggr.job_result.v1.rs");
+    }
+
+    pub mod types {
+        pub mod v1 {
+            include!("guggr.job_result.types.v1.rs");
+        }
+    }
+}
