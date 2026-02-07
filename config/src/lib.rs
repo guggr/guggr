156652a8a83fb51da606809a5883d680f6fba373
niri81 <38499069+niri81@users.noreply.github.com>
2026-02-07 17:59:32 +0100

    test(config): fix test for empty vhost

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -184,58 +184,55 @@ mod tests {
 
     #[test]
     fn vhost_empty() {
-        #[test]
-        fn valid_variables() {
-            let env_vars = vec![
-                ("RABBITMQ_USER", Some("user")),
-                ("RABBITMQ_PASSWORD", Some("password")),
-                ("RABBITMQ_HOST", Some("host")),
-                ("RABBITMQ_PORT", Some("port")),
-                ("RABBITMQ_VHOST", None),
-                ("queue_a", Some("a")),
-                ("queue_b", Some("b")),
-            ];
+        let env_vars = vec![
+            ("RABBITMQ_USER", Some("user")),
+            ("RABBITMQ_PASSWORD", Some("password")),
+            ("RABBITMQ_HOST", Some("host")),
+            ("RABBITMQ_PORT", Some("port")),
+            ("RABBITMQ_VHOST", None),
+            ("queue_a", Some("a")),
+            ("queue_b", Some("b")),
+        ];
 
-            temp_env::with_vars(env_vars, || {
-                let config = Config::from_env(&["queue_a", "queue_b"]).unwrap();
-                assert_eq!(
-                    config,
-                    Config {
-                        rabbitmq_user: "user".to_owned(),
-                        rabbitmq_password: "password".to_owned(),
-                        rabbitmq_host: "host".to_owned(),
-                        rabbitmq_port: "port".to_owned(),
-                        rabbitmq_vhost: None,
+        temp_env::with_vars(env_vars, || {
+            let config = Config::from_env(&["queue_a", "queue_b"]).unwrap();
+            assert_eq!(
+                config,
+                Config {
+                    rabbitmq_user: "user".to_owned(),
+                    rabbitmq_password: "password".to_owned(),
+                    rabbitmq_host: "host".to_owned(),
+                    rabbitmq_port: "port".to_owned(),
+                    rabbitmq_vhost: None,
 
-                        rabbitmq_queue_names: vec!["a".to_owned(), "b".to_owned()]
-                    }
-                );
+                    rabbitmq_queue_names: vec!["a".to_owned(), "b".to_owned()]
+                }
+            );
 
-                assert_eq!(config.rabbitmq_user(), "user");
-                assert_eq!(config.rabbitmq_password(), "password");
-                assert_eq!(config.rabbitmq_host(), "host");
-                assert_eq!(config.rabbitmq_port(), "port");
-                assert_eq!(config.rabbitmq_vhost(), None);
-                assert_eq!(
-                    config.connection_url(false),
-                    "amqp://user:password@host:port/%2f"
-                );
-                assert_eq!(
-                    config.connection_url(true),
-                    "amqps://user:password@host:port/%2f"
-                );
+            assert_eq!(config.rabbitmq_user(), "user");
+            assert_eq!(config.rabbitmq_password(), "password");
+            assert_eq!(config.rabbitmq_host(), "host");
+            assert_eq!(config.rabbitmq_port(), "port");
+            assert_eq!(config.rabbitmq_vhost(), None);
+            assert_eq!(
+                config.connection_url(false),
+                "amqp://user:password@host:port/%2f"
+            );
+            assert_eq!(
+                config.connection_url(true),
+                "amqps://user:password@host:port/%2f"
+            );
 
-                assert_eq!(
-                    config.rabbitmq_queue_names(),
-                    vec!["a".to_owned(), "b".to_owned()]
-                );
-                assert_eq!(config.rabbitmq_queue_name(0), Some("a".to_owned()));
-                assert_eq!(config.rabbitmq_queue_name(1), Some("b".to_owned()));
-                assert_eq!(config.rabbitmq_queue_name(2), None);
-                assert_eq!(config.rabbitmq_queue_name(3), None);
-                assert_eq!(config.rabbitmq_queue_name(99), None);
-            })
-        }
+            assert_eq!(
+                config.rabbitmq_queue_names(),
+                vec!["a".to_owned(), "b".to_owned()]
+            );
+            assert_eq!(config.rabbitmq_queue_name(0), Some("a".to_owned()));
+            assert_eq!(config.rabbitmq_queue_name(1), Some("b".to_owned()));
+            assert_eq!(config.rabbitmq_queue_name(2), None);
+            assert_eq!(config.rabbitmq_queue_name(3), None);
+            assert_eq!(config.rabbitmq_queue_name(99), None);
+        })
     }
 
     #[test]
