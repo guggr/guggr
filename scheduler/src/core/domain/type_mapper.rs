05f49addf01b750877647292e1b3ada6269a02b7
niri81 <38499069+niri81@users.noreply.github.com>
2026-02-09 18:18:55 +0100

    feat(scheduler): add mapper for database types to proto

---
+++ scheduler/src/core/domain/type_mapper.rs
@@ -0,0 +1,48 @@
+use database_client::models as db_models;
+use gen_proto_types::job::{
+    types::v1::{HttpJobType, PingJobType},
+    v1::{Job, JobType},
+};
+
+pub trait FromDatabaseType<F> {
+    fn from_database_type(value: F) -> Self;
+}
+
+impl FromDatabaseType<String> for JobType {
+    fn from_database_type(job_type_id: String) -> Self {
+        match job_type_id.as_str() {
+            "http" => JobType::Http,
+            "ping" => JobType::Ping,
+            _ => JobType::Unspecified,
+        }
+    }
+}
+
+impl FromDatabaseType<db_models::JobDetailsHttp> for HttpJobType {
+    fn from_database_type(value: db_models::JobDetailsHttp) -> Self {
+        Self { url: value.url }
+    }
+}
+
+impl FromDatabaseType<db_models::JobDetailsPing> for PingJobType {
+    fn from_database_type(value: db_models::JobDetailsPing) -> Self {
+        Self { host: value.host }
+    }
+}
+
+pub type DatabaseJobResult = (
+    db_models::Job,
+    Option<db_models::JobDetailsHttp>,
+    Option<db_models::JobDetailsPing>,
+);
+
+impl FromDatabaseType<DatabaseJobResult> for Job {
+    fn from_database_type(value: DatabaseJobResult) -> Self {
+        Self {
+            id: value.0.id,
+            job_type: JobType::from_database_type(value.0.job_type_id).into(),
+            http: value.1.map(FromDatabaseType::from_database_type),
+            ping: value.2.map(FromDatabaseType::from_database_type),
+        }
+    }
+}
