16f7219379b03102c596752834698c7091e4c134
niri81 <38499069+niri81@users.noreply.github.com>
2026-02-09 19:50:27 +0100

    feat(scheduler): implement graceful shutdown for ticker

--- scheduler/src/adapters/inbound/ticker.rs
+++ scheduler/src/adapters/inbound/ticker.rs
@@ -2,6 +2,8 @@ use std::{sync::Arc, time::Duration};
 
 use async_trait::async_trait;
 use tokio::time::{Interval, MissedTickBehavior};
+use tokio_util::{sync::CancellationToken, task::TaskTracker};
+use tracing::info;
 
 use crate::core::{
     domain::errors::JobSchedulerError,
@@ -12,13 +14,21 @@ use crate::core::{
 pub struct SchedulerTicker {
     service: Arc<SchedulerService>,
     interval: Interval,
+    task_tracker: TaskTracker,
+    shutdown_token: CancellationToken,
 }
 
 impl SchedulerTicker {
-    pub fn new(service: Arc<SchedulerService>, run_every: Duration) -> Self {
+    pub fn new(
+        service: Arc<SchedulerService>,
+        run_every: Duration,
+        shutdown_token: CancellationToken,
+    ) -> Self {
         Self {
             service,
             interval: tokio::time::interval(run_every),
+            task_tracker: TaskTracker::new(),
+            shutdown_token,
         }
     }
 }
@@ -30,28 +40,43 @@ impl Ticker for SchedulerTicker {
         interval.set_missed_tick_behavior(MissedTickBehavior::Skip);
 
         loop {
-            interval.tick().await;
-            let service = Arc::clone(&self.service);
+            tokio::select! {
+                _ = interval.tick() => {
+                    let service = Arc::clone(&self.service);
 
-            tokio::spawn(async move {
-                match service.run().await {
-                    Ok(_) => tracing::debug!("Batch processed successfully."),
+                    self.task_tracker.spawn(async move {
+                        match service.run().await {
+                            Ok(_) => tracing::debug!("Batch processed successfully."),
 
-                    Err(JobSchedulerError::DatabaseUnavailable(e)) => {
-                        // We use WARN here because it's a transient infra issue
-                        tracing::warn!("Postgres is unreachable. Skipping this tick. {e}");
-                    }
+                            Err(JobSchedulerError::DatabaseUnavailable(e)) => {
+                                // We use WARN here because it's a transient infra issue
+                                tracing::warn!("Postgres is unreachable. Skipping this tick. {e}");
+                            }
 
-                    Err(JobSchedulerError::QueueUnavailable(e)) => {
-                        // We use ERROR because jobs were fetched but couldn't be sent
-                        tracing::error!("RabbitMQ is down. Failed to dispatch jobs! {e}");
-                    }
+                            Err(JobSchedulerError::QueueUnavailable(e)) => {
+                                // We use ERROR because jobs were fetched but couldn't be sent
+                                tracing::error!("RabbitMQ is down. Failed to dispatch jobs! {e}");
+                            }
 
-                    Err(e) => {
-                        tracing::error!("Critical/Unknown error in service: {e}");
-                    }
+                            Err(e) => {
+                                tracing::error!("Critical/Unknown error in service: {e}");
+                            }
+                        }
+                    });
                 }
-            });
+                _ = self.shutdown_token.cancelled() => {
+                    info!("shutdown token cancelled, exiting ticker");
+                    break;
+                }
+
+            }
         }
+
+        self.task_tracker.close();
+
+        info!("Waiting for in-flight jobs to complete...");
+        self.task_tracker.wait().await;
+
+        info!("all jobs finished")
     }
 }
