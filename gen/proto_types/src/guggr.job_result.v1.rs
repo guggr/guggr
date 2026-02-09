673357c2f4982eb2c2fe16451bc335b746339b9d
niri81 <38499069+niri81@users.noreply.github.com>
2026-02-09 21:45:40 +0100

    feat(proto): add batch ids to job and result

--- gen/proto_types/src/guggr.job_result.v1.rs
+++ gen/proto_types/src/guggr.job_result.v1.rs
@@ -5,6 +5,8 @@ pub struct JobResult {
     pub id: ::prost::alloc::string::String,
     #[prost(message, optional, tag = "2")]
     pub timestamp: ::core::option::Option<::protocheck::types::Timestamp>,
+    #[prost(string, tag = "3")]
+    pub batch_id: ::prost::alloc::string::String,
     #[prost(message, optional, tag = "16")]
     pub http: ::core::option::Option<super::types::v1::HttpJobResult>,
     #[prost(message, optional, tag = "17")]
