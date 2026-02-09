fc7baac65565f2db4dc223d1f0e1146d4e8a91a6
niri81 <38499069+niri81@users.noreply.github.com>
2026-02-09 19:06:25 +0100

    feat(scheduler): add scheduler service

---
+++ scheduler/src/core/service/schedulerservice.rs
@@ -0,0 +1,44 @@
+use std::sync::Arc;
+
+use gen_proto_types::job::v1::{Job, JobType};
+use tracing::error;
+
+use crate::core::{
+    domain::{errors::JobSchedulerError, type_mapper::FromDatabaseType},
+    ports::{job_fetcher::JobFetcher, publisher::Publisher},
+};
+
+pub struct SchedulerService {
+    repo: Arc<dyn JobFetcher>,
+    publisher: Arc<dyn Publisher>,
+}
+
+impl SchedulerService {
+    pub fn new(repo: Arc<dyn JobFetcher>, publisher: Arc<dyn Publisher>) -> Self {
+        Self { repo, publisher }
+    }
+
+    pub async fn run(&self) -> Result<(), JobSchedulerError> {
+        let jobs = self.repo.fetch_jobs_batch().await?;
+
+        if jobs.is_empty() {
+            return Ok(());
+        }
+
+        for job in jobs {
+            let job = Job::from_database_type(job);
+
+            if job.job_type() == JobType::Unspecified {
+                error!("Encountered unknown job type in job id {}", job.id);
+                continue;
+            }
+
+            match self.publisher.publish(job).await {
+                Ok(..) => (),
+                Err(e) => error!("Failed to publish job: {:?}", e),
+            }
+        }
+
+        Ok(())
+    }
+}
