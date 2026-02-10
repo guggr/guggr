61610c2a23970253bcba79f15fc003559639929d
niri81 <38499069+niri81@users.noreply.github.com>
2026-02-10 13:01:54 +0100

    fix(scheduler): add error output for domain error
    Co-authored-by: philxws692 <63463378+philxws692@users.noreply.github.com>

--- scheduler/src/core/domain/errors.rs
+++ scheduler/src/core/domain/errors.rs
@@ -2,7 +2,7 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum JobSchedulerError {
-    #[error("Database is currently unavailable")]
+    #[error("Database is currently unavailable: {0}")]
     DatabaseUnavailable(String),
     #[error("Queue is currently unavailable: {0}")]
     QueueUnavailable(String),
