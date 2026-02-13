12fcd08af34d4693425255ab75235c99851d42ea
melo-afk <git@melo.run>
2026-02-13 16:57:02 +0100

    Merge branch 'main' into feat/evaluator-rs

---
+++ database/migrations/2026-02-12-233256-0000_add-result-job-tables/down.sql
@@ -0,0 +1,10 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE public.job_result_ping;
+
+DROP TABLE public.job_result_http;
+
+ALTER TABLE public.job_runs
+DROP COLUMN reachable;
+
+ALTER TABLE public.job_runs
+ADD COLUMN output TEXT;
