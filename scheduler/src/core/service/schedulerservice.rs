78bbcd69011525c251d6f25ff79d43eafe8ee05f
niri81 <38499069+niri81@users.noreply.github.com>
2026-02-14 13:19:14 +0100

    test(scheduler): add service tests

--- scheduler/src/core/service/schedulerservice.rs
+++ scheduler/src/core/service/schedulerservice.rs
@@ -74,3 +74,186 @@ impl SchedulerService {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use std::sync::{
+        Mutex,
+        atomic::{AtomicUsize, Ordering},
+    };
+
+    use async_trait::async_trait;
+    use database_client::models::Job;
+    use diesel::data_types::PgInterval;
+
+    use super::*;
+    use crate::{core::domain::type_mapper::DatabaseJobResult, telemetry::init_tracing};
+
+    fn mock_job() -> DatabaseJobResult {
+        (
+            Job {
+                id: "".to_owned(),
+                group_id: "".to_owned(),
+                job_type_id: "http".to_owned(),
+                name: "".to_owned(),
+                last_scheduled: None,
+                notify_users: false,
+                run_every: PgInterval {
+                    months: 0,
+                    days: 0,
+                    microseconds: 5000,
+                },
+                custom_notification: None,
+            },
+            None,
+            None,
+        )
+    }
+
+    struct MockFetcher {
+        jobs: Mutex<Option<Vec<DatabaseJobResult>>>,
+    }
+
+    impl MockFetcher {
+        fn new(jobs: Vec<DatabaseJobResult>) -> Self {
+            Self {
+                jobs: Mutex::new(Some(jobs)),
+            }
+        }
+    }
+
+    #[async_trait]
+    impl JobFetcher for MockFetcher {
+        async fn fetch_jobs_batch(&self) -> Result<Vec<DatabaseJobResult>, JobSchedulerError> {
+            let mut guard = self.jobs.lock().unwrap();
+
+            // .take() returns Some(jobs) the first time, and None every time after
+            let jobs = guard.take().ok_or_else(|| {
+                JobSchedulerError::Internal(
+                    "Mock Jobs already taken/Mock only supports one call".into(),
+                )
+            })?;
+
+            Ok(jobs)
+        }
+    }
+
+    struct MockPublisher {
+        error_at: Option<usize>,
+        counter: AtomicUsize,
+        published_counter: AtomicUsize,
+    }
+
+    impl MockPublisher {
+        fn new(error_at: Option<usize>) -> Self {
+            Self {
+                error_at,
+                counter: AtomicUsize::new(0),
+                published_counter: AtomicUsize::new(0),
+            }
+        }
+
+        fn published_jobs(&self) -> usize {
+            self.published_counter.load(Ordering::SeqCst)
+        }
+
+        fn failing_jobs(&self) -> usize {
+            self.counter.load(Ordering::SeqCst) - self.published_jobs()
+        }
+    }
+
+    #[async_trait]
+    impl Publisher for MockPublisher {
+        async fn publish(
+            &self,
+            _job: gen_proto_types::job::v1::Job,
+        ) -> Result<(), JobSchedulerError> {
+            let curr_counter = self.counter.load(Ordering::SeqCst);
+
+            if self.error_at.is_some_and(|x| x == curr_counter) {
+                self.counter.fetch_add(1, Ordering::SeqCst);
+                return Err(JobSchedulerError::Internal(
+                    "Triggered requested error".to_owned(),
+                ));
+            }
+
+            self.counter.fetch_add(1, Ordering::SeqCst);
+            self.published_counter.fetch_add(1, Ordering::SeqCst);
+            Ok(())
+        }
+    }
+
+    fn init() {
+        init_tracing();
+    }
+
+    #[tokio::test]
+    async fn no_errors() {
+        init();
+
+        let mut jobs = Vec::with_capacity(12);
+
+        for _ in 0..15 {
+            jobs.push(mock_job());
+        }
+
+        let fetcher = Arc::from(MockFetcher::new(jobs));
+        let publisher = Arc::from(MockPublisher::new(None));
+
+        let service = SchedulerService::new(fetcher.clone(), publisher.clone());
+
+        assert!(service.run().await.is_ok());
+        assert_eq!(publisher.published_jobs(), 15);
+        assert_eq!(publisher.failing_jobs(), 0);
+    }
+
+    #[tokio::test]
+    async fn unspecified_job_type() {
+        init();
+
+        let mut unspecified_job_type = mock_job();
+        unspecified_job_type.0.job_type_id = "doesnotexist".to_owned();
+
+        let fetcher = Arc::from(MockFetcher::new(vec![
+            mock_job(),
+            unspecified_job_type,
+            mock_job(),
+        ]));
+        let publisher = Arc::from(MockPublisher::new(None));
+
+        let service = SchedulerService::new(fetcher.clone(), publisher.clone());
+
+        assert!(service.run().await.is_ok());
+        assert_eq!(publisher.published_jobs(), 2);
+        assert_eq!(publisher.failing_jobs(), 0);
+    }
+
+    #[tokio::test]
+    async fn no_pending_jobs() {
+        init();
+
+        let fetcher = Arc::from(MockFetcher::new(vec![]));
+        let publisher = Arc::from(MockPublisher::new(None));
+
+        let service = SchedulerService::new(fetcher.clone(), publisher.clone());
+
+        assert!(service.run().await.is_ok());
+        assert_eq!(publisher.published_jobs(), 0);
+        assert_eq!(publisher.failing_jobs(), 0);
+    }
+
+    #[tokio::test]
+    async fn publishing_error() {
+        init();
+
+        let fetcher = Arc::from(MockFetcher::new(vec![mock_job(), mock_job(), mock_job()]));
+        let publisher = Arc::from(MockPublisher::new(Some(1)));
+
+        let service = SchedulerService::new(fetcher.clone(), publisher.clone());
+
+        assert!(service.run().await.is_ok());
+        assert_eq!(publisher.published_jobs(), 2);
+        assert_eq!(publisher.failing_jobs(), 1);
+    }
+}
