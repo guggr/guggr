c040ea1b73c7fbb1d606f40a254a4917dff34bc3
tametsi <93092155+tametsi@users.noreply.github.com>
2026-02-17 12:25:43 +0100

    feat(ui): add login/register mockup

---
+++ frontend/web/src/components/account/LoginCard.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import FormCard from '@/components/shared/FormCard.svelte';
+
+	let email = $state(''),
+		password = $state('');
+
+	const login = () => {
+		// TODO
+	};
+</script>
+
+<FormCard title="Login" actionTitle="Login" onsubmit={login} {subtext}>
+	<label>
+		<span class="label my-1">E-Mail</span>
+		<input
+			type="email"
+			bind:value={email}
+			required
+			class="input validator"
+			placeholder="Email"
+		/>
+	</label>
+
+	<label class="my-1">
+		<span class="label my-1">Password</span>
+		<input
+			type="password"
+			bind:value={password}
+			required
+			class="input validator"
+			placeholder="Password"
+		/>
+	</label>
+</FormCard>
+
+{#snippet subtext()}
+	New to guggr? <a href="/register" class="link">Create an account!</a>
+{/snippet}
