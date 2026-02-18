15fc852f832d372c9c173453524e91c37b2a02dd
melo-afk <git@melo.run>
2026-02-18 19:06:41 +0100

    feat(api-service): add example usage

--- api-service/src/lib.rs
+++ api-service/src/lib.rs
@@ -1,3 +1,40 @@
 pub mod adapters;
 pub mod core;
 pub mod telemetry;
+
+use database_client::models::Group;
+
+use crate::{adapters::outgoing::postgres::PostgresAdapter, core::ports::storage::Crud};
+
+/// TODO delete this
+pub async fn example_usage(postgres: PostgresAdapter) -> anyhow::Result<()> {
+    let group = Group {
+        id: "mycoolgroup".to_string(),
+        name: "This group is cool".to_string(),
+    };
+    postgres.create(group).await?;
+
+    let r: Option<Group> = postgres.get_by_id("mycoolgroup").await?;
+    dbg!(r);
+
+    let updategroup = Group {
+        id: "mycoolgroup".to_string(),
+        name: "other description".to_string(),
+    };
+
+    postgres.update(updategroup).await?;
+
+    let r: Option<Group> = postgres.get_by_id("mycoolgroup").await?;
+    dbg!(r);
+
+    let entries: Vec<Group> = postgres.list(5).await?;
+    dbg!(entries);
+
+    // this is kind of ugly, need to check how this can be done better
+    <PostgresAdapter as Crud<Group>>::delete(&postgres, "mycoolgroup").await?;
+
+    let r: Option<Group> = postgres.get_by_id("mycoolgroup").await?;
+    dbg!(r);
+
+    Ok(())
+}
--- api-service/src/main.rs
+++ api-service/src/main.rs
@@ -1,13 +1,18 @@
 use anyhow::Result;
-use api_service::{adapters::outgoing::postgres::PostgresAdapter, telemetry::init_tracing};
+use api_service::{
+    adapters::outgoing::postgres::PostgresAdapter, example_usage, telemetry::init_tracing,
+};
 use config::PostgresConfig;
 use tracing::debug;
 
-fn main() -> Result<()> {
+#[actix_web::main]
+async fn main() -> Result<()> {
     init_tracing();
     let config = PostgresConfig::from_env()?;
     debug!("initializing postgres adapter and running pending migrations on the database");
-    let _postgres = PostgresAdapter::new(&config.postgres_connection_url())?;
+    let postgres = PostgresAdapter::new(&config.postgres_connection_url())?;
+
+    example_usage(postgres).await?;
 
     Ok(())
 }
