6a2085a3ba98ea0e521d2b61e95759e7d0c606e3
melo-afk <git@melo.run>
2026-02-21 17:06:28 +0100

    refactor(db-client): reduce derives
    Removed serde and utoipa Derives and added frunk's LabelledGeneric

--- database/database-client/src/models.rs
+++ database/database-client/src/models.rs
@@ -5,45 +5,20 @@
 
 use chrono::{Duration, NaiveDateTime};
 use diesel::prelude::*;
+use frunk::LabelledGeneric;
 use ipnet::IpNet;
-use serde::{Deserialize, Serialize};
-use utoipa::ToSchema;
 
 use crate::schema::*;
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = group)]
 pub struct Group {
-    #[serde(default)]
     pub id: String,
     pub name: String,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = job)]
 pub struct Job {
-    #[serde(default)]
     pub id: String,
     pub name: String,
     pub job_type_id: String,
@@ -54,103 +29,41 @@ pub struct Job {
     pub last_scheduled: Option<NaiveDateTime>,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = job_details_http)]
 pub struct JobDetailsHttp {
-    #[serde(default)]
     pub id: String,
     pub url: String,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = job_details_ping)]
 pub struct JobDetailsPing {
-    #[serde(default)]
     pub id: String,
     pub host: String,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = job_result_http)]
 pub struct JobResultHttp {
-    #[serde(default)]
     pub id: String,
-    #[schema(value_type = String, example = "10.0.0.0")]
     pub ip_address: IpNet,
     pub status_code: i32,
     pub latency: i32,
     pub payload: Vec<u8>,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = job_result_ping)]
 pub struct JobResultPing {
-    #[serde(default)]
     pub id: String,
-    #[schema(value_type = String, example = "10.0.0.0")]
     pub ip_address: IpNet,
     pub latency: i32,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = job_runs)]
 pub struct JobRun {
-    #[serde(default)]
     pub id: String,
     pub job_id: String,
     pub timestamp: NaiveDateTime,
@@ -159,77 +72,30 @@ pub struct JobRun {
     pub reachable: bool,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = job_type)]
 pub struct JobType {
-    #[serde(default)]
     pub id: String,
     pub name: Option<String>,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = role)]
 pub struct Role {
-    #[serde(default)]
     pub id: String,
     pub name: String,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(table_name = user)]
 pub struct User {
-    #[serde(default)]
     pub id: String,
     pub name: String,
     pub email: String,
     pub password: String,
 }
 
-#[derive(
-    Queryable,
-    Debug,
-    Identifiable,
-    Insertable,
-    PartialEq,
-    Eq,
-    Clone,
-    Serialize,
-    Deserialize,
-    ToSchema,
-)]
+#[derive(Queryable, Debug, Identifiable, Insertable, PartialEq, Eq, Clone, LabelledGeneric)]
 #[diesel(primary_key(user_id, group_id))]
 #[diesel(table_name = user_group_mapping)]
 pub struct UserGroupMapping {
