472e1718458c92be8daaa8c80c132d47d702ec48
melo-afk <git@melo.run>
2026-02-22 21:09:18 +0100

    feat(config): add basic api-service config

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -2,16 +2,22 @@ mod basic;
 mod connection_url;
 
 mod agent;
+mod api_service;
 mod postgres;
 mod rabbitmq;
 
-use std::env;
+use std::{env, num::ParseIntError};
 
 pub use agent::AgentConfig;
+pub use api_service::ApiServiceConfig;
 pub use postgres::PostgresConfig;
 pub use rabbitmq::RabbitMQConfig;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-#[error("Error while loading config variable")]
-pub struct ConfigError(#[from] env::VarError);
+pub enum ConfigError {
+    #[error("Error while loading config variable")]
+    Env(#[from] env::VarError),
+    #[error("Error while parsing integer")]
+    ParseInt(#[from] ParseIntError),
+}
