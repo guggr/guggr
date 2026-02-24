7f217431cf87fe36872fea512bbdf7c06c2196ba
niri81 <38499069+niri81@users.noreply.github.com>
2026-02-25 00:19:30 +0100

    refactor(api-service): share app init for openapi gen and http server
    This does not require both to be adapted anymore.

--- api-service/src/gen_openapi.rs
+++ api-service/src/gen_openapi.rs
@@ -1,13 +1,6 @@
 use std::{fs::File, io::Write};
 
-use actix_web::App;
-use api_service::{
-    adapters::inbound::http::{self, auth, groups, jobs, roles, users},
-    core::domain::openapi_helper::ApiDoc,
-};
-use tracing_actix_web::TracingLogger;
-use utoipa::OpenApi;
-use utoipa_actix_web::{self, AppExt};
+use api_service::init_app_openapi;
 
 fn main() -> anyhow::Result<()> {
     let args: Vec<String> = std::env::args().collect();
@@ -18,20 +11,7 @@ fn main() -> anyhow::Result<()> {
         "api-service/openapi.json".to_owned()
     };
 
-    let (_, a) = App::new()
-        .wrap(TracingLogger::default())
-        .into_utoipa_app()
-        .openapi(ApiDoc::openapi())
-        .service(
-            utoipa_actix_web::scope("/api/v1")
-                .configure(groups::configure)
-                .configure(users::configure)
-                .configure(jobs::configure)
-                .configure(roles::configure)
-                .configure(auth::configure)
-                .configure(http::configure),
-        )
-        .split_for_parts();
+    let (_, a) = init_app_openapi(None, None, false);
 
     let mut file = File::create(path)?;
     file.write_all(a.to_pretty_json()?.as_bytes())?;
--- api-service/src/lib.rs
+++ api-service/src/lib.rs
@@ -2,13 +2,80 @@ pub mod adapters;
 pub mod core;
 pub mod telemetry;
 
+use std::sync::Arc;
+
+use actix_web::{App, dev::ServiceFactory, web::Data};
+use config::ApiServiceConfig;
+use tracing_actix_web::TracingLogger;
+use utoipa::OpenApi;
+use utoipa_actix_web::AppExt;
+use utoipa_swagger_ui::SwaggerUi;
+
 use crate::{
-    adapters::outgoing::postgres::PostgresAdapter,
+    adapters::{
+        inbound::http::{self, auth, groups, jobs, roles, users},
+        outgoing::postgres::PostgresAdapter,
+    },
     core::{
+        domain::openapi_helper::ApiDoc,
         models::group::{CreateGroup, DisplayGroup, UpdateGroup},
-        ports::storage::CrudOperations,
+        ports::storage::{CrudOperations, StoragePort},
     },
 };
+
+/// Initializes and returns configured Actix [`App`] and corresponding
+/// [`utoipa::OpenApi`] specification.
+///
+/// If `api` and `dconfig` options are set, they will be propagated as app data.
+/// If `enable_swagger_ui` bool is set to `true`, a Swagger UI endpoint will be
+/// available at `/api/swagger-ui/`.
+#[allow(clippy::type_complexity)]
+pub fn init_app_openapi(
+    api: Option<Data<Arc<dyn StoragePort>>>,
+    dconfig: Option<Data<ApiServiceConfig>>,
+    enable_swagger_ui: bool,
+) -> (
+    App<
+        impl ServiceFactory<
+            actix_web::dev::ServiceRequest,
+            Config = (),
+            Response = actix_web::dev::ServiceResponse<
+                tracing_actix_web::StreamSpan<actix_web::body::BoxBody>,
+            >,
+            Error = actix_web::Error,
+            InitError = (),
+        >,
+    >,
+    utoipa::openapi::OpenApi,
+) {
+    let mut app = App::new()
+        .wrap(TracingLogger::default())
+        .into_utoipa_app()
+        .openapi(ApiDoc::openapi())
+        .service(
+            utoipa_actix_web::scope("/api/v1")
+                .configure(groups::configure)
+                .configure(users::configure)
+                .configure(jobs::configure)
+                .configure(roles::configure)
+                .configure(auth::configure)
+                .configure(http::configure),
+        );
+
+    if let Some(api) = api {
+        app = app.app_data(api.clone());
+    }
+    if let Some(dconfig) = dconfig {
+        app = app.app_data(dconfig.clone());
+    }
+    if enable_swagger_ui {
+        app = app.openapi_service(|api| {
+            SwaggerUi::new("/api/swagger-ui/{_:.*}").url("/api/openapi.json", api)
+        })
+    }
+
+    app.split_for_parts()
+}
 
 /// TODO delete this
 pub async fn example_usage(postgres: PostgresAdapter) -> anyhow::Result<()> {
