2cf4ac1a6f0b94ff1fb74adb879ef7f73e700add
melo-afk <git@melo.run>
2026-02-25 17:50:03 +0100

    feat(api-service): implement further auth checks for user access

--- api-service/src/adapters/inbound/http/users.rs
+++ api-service/src/adapters/inbound/http/users.rs
@@ -1,7 +1,8 @@
 use std::sync::Arc;
 
 use actix_web::{
-    HttpResponse, Responder, delete, error::ErrorInternalServerError, get, patch, post, web,
+    HttpMessage, HttpRequest, HttpResponse, Responder, delete, error::ErrorInternalServerError,
+    get, patch, post, web,
 };
 use garde_actix_web::web::Json;
 use utoipa_actix_web::service_config::ServiceConfig;
@@ -9,8 +10,14 @@ use utoipa_actix_web::service_config::ServiceConfig;
 use crate::{
     adapters::inbound::http::middleware::auth::Auth,
     core::{
-        domain::openapi_helper::{GenericResponses, GenericResponsesCU},
-        models::user::{CreateUser, DisplayUser, UpdateUser},
+        domain::{
+            errors::{AuthError, StorageError},
+            openapi_helper::{GenericResponses, GenericResponsesCU},
+        },
+        models::{
+            auth::UserId,
+            user::{CreateUser, DisplayUser, UpdateUser},
+        },
         ports::storage::StoragePort,
     },
 };
@@ -43,10 +50,21 @@ pub fn configure(cfg: &mut ServiceConfig) {
 pub async fn create(
     api: web::Data<Arc<dyn StoragePort>>,
     body: Json<CreateUser>,
+    req: HttpRequest,
 ) -> actix_web::Result<impl Responder> {
-    let user = web::block(move || api.user().create(body.into_inner()))
-        .await
-        .map_err(ErrorInternalServerError)??;
+    let req_userid = req
+        .extensions()
+        .get::<UserId>()
+        .cloned()
+        .ok_or(AuthError::Unauthorized)?;
+    let user = web::block(move || {
+        if api.auth().is_owner(&req_userid.0)? {
+            return api.user().create(body.into_inner());
+        }
+        Err(StorageError::Unauthorized)
+    })
+    .await
+    .map_err(ErrorInternalServerError)??;
     Ok(HttpResponse::Ok().json(user))
 }
 
@@ -61,10 +79,28 @@ pub async fn create(
 )]
 #[get("")]
 /// list endpoint for users
-pub async fn list(api: web::Data<Arc<dyn StoragePort>>) -> actix_web::Result<impl Responder> {
-    let users = web::block(move || api.user().list(5))
-        .await
-        .map_err(ErrorInternalServerError)??;
+pub async fn list(
+    api: web::Data<Arc<dyn StoragePort>>,
+    req: HttpRequest,
+) -> actix_web::Result<impl Responder> {
+    let req_userid = req
+        .extensions()
+        .get::<UserId>()
+        .cloned()
+        .ok_or(AuthError::Unauthorized)?;
+    let users = web::block(move || {
+        if api.auth().is_owner(&req_userid.0)? {
+            api.user().list(5)
+        } else {
+            let u = api.user().get_by_id(&req_userid.0)?;
+            if let Some(user) = u {
+                return Ok(vec![user]);
+            }
+            Ok(vec![])
+        }
+    })
+    .await
+    .map_err(ErrorInternalServerError)??;
     Ok(HttpResponse::Ok().json(users))
 }
 
@@ -85,14 +121,26 @@ pub async fn list(api: web::Data<Arc<dyn StoragePort>>) -> actix_web::Result<imp
 pub async fn get(
     api: web::Data<Arc<dyn StoragePort>>,
     path: web::Path<String>,
+    req: HttpRequest,
 ) -> actix_web::Result<impl Responder> {
-    web::block(move || api.user().get_by_id(&path.into_inner()))
-        .await
-        .map_err(ErrorInternalServerError)??
-        .map_or_else(
-            || Ok(HttpResponse::NotFound().finish()),
-            |user| Ok(HttpResponse::Ok().json(user)),
-        )
+    let req_userid = req
+        .extensions()
+        .get::<UserId>()
+        .cloned()
+        .ok_or(AuthError::Unauthorized)?;
+    web::block(move || {
+        let id = path.into_inner();
+        if api.auth().is_owner(&req_userid.0)? || req_userid.0 == id {
+            return api.user().get_by_id(&id);
+        }
+        Err(StorageError::Unauthorized)
+    })
+    .await
+    .map_err(ErrorInternalServerError)??
+    .map_or_else(
+        || Ok(HttpResponse::NotFound().finish()),
+        |user| Ok(HttpResponse::Ok().json(user)),
+    )
 }
 
 #[utoipa::path(
@@ -115,10 +163,22 @@ pub async fn update(
     api: web::Data<Arc<dyn StoragePort>>,
     path: web::Path<String>,
     body: Json<UpdateUser>,
+    req: HttpRequest,
 ) -> actix_web::Result<impl Responder> {
-    let user = web::block(move || api.user().update(&path.into_inner(), body.into_inner()))
-        .await
-        .map_err(ErrorInternalServerError)??;
+    let req_userid = req
+        .extensions()
+        .get::<UserId>()
+        .cloned()
+        .ok_or(AuthError::Unauthorized)?;
+    let user = web::block(move || {
+        let id = path.into_inner();
+        if api.auth().is_owner(&req_userid.0)? || req_userid.0 == id {
+            return api.user().update(&id, body.into_inner());
+        }
+        Err(StorageError::Unauthorized)
+    })
+    .await
+    .map_err(ErrorInternalServerError)??;
     Ok(HttpResponse::Ok().json(user))
 }
 #[utoipa::path(
@@ -139,10 +199,21 @@ pub async fn update(
 pub async fn delete(
     api: web::Data<Arc<dyn StoragePort>>,
     path: web::Path<String>,
+    req: HttpRequest,
 ) -> actix_web::Result<impl Responder> {
-    let id = path.into_inner();
-    web::block(move || api.user().delete(&id))
-        .await
-        .map_err(ErrorInternalServerError)??;
+    let req_userid = req
+        .extensions()
+        .get::<UserId>()
+        .cloned()
+        .ok_or(AuthError::Unauthorized)?;
+    web::block(move || {
+        let id = path.into_inner();
+        if api.auth().is_owner(&req_userid.0)? || req_userid.0 == id {
+            return api.user().delete(&id);
+        }
+        Err(StorageError::Unauthorized)
+    })
+    .await
+    .map_err(ErrorInternalServerError)??;
     Ok(HttpResponse::NoContent().finish())
 }
