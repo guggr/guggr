78a03e608f1dcff8c4f7f65bc05075056bfd1b80
tametsi <93092155+tametsi@users.noreply.github.com>
2026-03-01 01:14:49 +0100

    refactor: remove unused auth models

--- api-service/src/core/models/auth.rs
+++ api-service/src/core/models/auth.rs
@@ -1,10 +1,8 @@
-use chrono::{DateTime, TimeZone, Utc};
-use database_client::models::RefreshToken;
 use frunk::LabelledGeneric;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
-use crate::core::{domain::errors::DomainError, models::user::DisplayUser};
+use crate::core::models::user::DisplayUser;
 
 #[derive(Debug, Deserialize, ToSchema)]
 /// Data used to authenticate.
@@ -42,59 +40,3 @@ pub struct TokenResponse {
 #[derive(Debug, Serialize, ToSchema, LabelledGeneric, Clone)]
 /// Passing `user_id` from the middleware to endpoint handlers path
 pub struct UserId(pub String);
-
-// DB DTO models below
-
-#[derive(Debug, LabelledGeneric, Default)]
-/// returned from the database on a user login
-pub struct UserAuth {
-    pub id: String,
-    pub email: String,
-    /// this is a argon2id hash
-    pub password: String,
-    pub jwt_secret: Vec<u8>,
-}
-
-#[derive(Debug, LabelledGeneric, Default)]
-/// returned from the database on an auth check
-pub struct UserAuthJwt {
-    pub jwt_secret: Vec<u8>,
-}
-
-#[derive(Debug, Default)]
-/// sent to the database for creating a new `RefreshToken` record
-pub struct CreateRefreshToken {
-    pub token: String,
-    pub user_id: String,
-    pub expires_on: i64,
-}
-
-#[derive(Debug, LabelledGeneric, Default)]
-/// returned from the database when an old refresh token was requested
-pub struct DisplayRefreshToken {
-    pub user_id: String,
-    pub expires_on: i64,
-}
-
-impl TryFrom<CreateRefreshToken> for RefreshToken {
-    type Error = DomainError;
-    fn try_from(value: CreateRefreshToken) -> Result<Self, Self::Error> {
-        let exp = DateTime::<Utc>::from_timestamp_secs(value.expires_on)
-            .ok_or(DomainError::TimestampConversion)?
-            .naive_utc();
-        Ok(Self {
-            token: value.token,
-            user_id: value.user_id,
-            expires_on: exp,
-        })
-    }
-}
-impl From<RefreshToken> for DisplayRefreshToken {
-    fn from(value: RefreshToken) -> Self {
-        let exp = Utc.from_utc_datetime(&value.expires_on).timestamp();
-        Self {
-            user_id: value.user_id,
-            expires_on: exp,
-        }
-    }
-}
