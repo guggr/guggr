846eb9c67b28d8feb2ef3290b263b1c0dfb7ad90
melo-afk <git@melo.run>
2026-03-02 16:40:04 +0100

    feat(api-service): add group repository

---
+++ api-service/src/adapters/outgoing/postgres/groups.rs
@@ -0,0 +1,44 @@
+use database_client::{
+    models::Group,
+    schema::{group, user_group_mapping},
+};
+use diesel::{ExpressionMethods, JoinOnDsl, QueryDsl, RunQueryDsl};
+
+use crate::{
+    adapters::outgoing::postgres::{Postgres, PostgresError},
+    core::{domain::errors::DomainError, ports::repository::RepositoryGroupPort},
+};
+
+impl RepositoryGroupPort for Postgres {
+    fn create_group(
+        &self,
+        new_group: database_client::models::Group,
+    ) -> Result<database_client::models::Group, DomainError> {
+        let mut conn = self.pool.get().map_err(PostgresError::from)?;
+
+        let result: Group = diesel::insert_into(group::dsl::group)
+            .values(new_group)
+            .get_result(&mut conn)
+            .map_err(PostgresError::from)?;
+
+        Ok(result)
+    }
+
+    fn list_groups_by_user_id(
+        &self,
+        user_id: &str,
+    ) -> Result<Vec<database_client::models::Group>, DomainError> {
+        let mut conn = self.pool.get().map_err(PostgresError::from)?;
+
+        let groups: Vec<Group> = group::table
+            .inner_join(user_group_mapping::table.on(user_group_mapping::group_id.eq(group::id)))
+            .filter(user_group_mapping::user_id.eq(user_id))
+            .select((group::id, group::name))
+            .distinct()
+            .order(group::name.asc())
+            .load::<Group>(&mut conn)
+            .map_err(PostgresError::from)?;
+
+        Ok(groups)
+    }
+}
