7196b39d9aa1ee13e8be4c96411a188d2d5d1ff1
tametsi <93092155+tametsi@users.noreply.github.com>
2026-03-03 12:51:14 +0100

    feat(ui): add registration logic

--- frontend/web/src/api/index.ts
+++ frontend/web/src/api/index.ts
@@ -1,1 +1,8 @@
+import { Configuration } from '@/api';
 export * from '@/api/gen';
+
+const BASE_PATH = import.meta.env.DEV ? 'http://localhost:8081' : '';
+
+export const config = new Configuration({
+	basePath: BASE_PATH,
+});
