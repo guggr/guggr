cc207b07a6ce4d156c40b58256abdba2c6fe6e8c
tametsi <93092155+tametsi@users.noreply.github.com>
2026-03-05 10:24:15 +0100

    refactor(ui): move group overview to higher svelte component
    This allows easy state passing of all available groups without the need
    to introduce global state.

---
+++ frontend/web/src/components/groups/GroupsOverview.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { config, GroupsApi, type DisplayGroup } from '@/api';
+	import GroupList from '@/components/groups/GroupList.svelte';
+	import Error from '@/components/shared/Error.svelte';
+	import Loading from '@/components/shared/Loading.svelte';
+	import alerts from '@/stores/alerts.svelte';
+	import { onMount } from 'svelte';
+
+	let groupsPromise = $state(new Promise<DisplayGroup[]>(() => {}));
+
+	onMount(() => {
+		const api = new GroupsApi(config);
+
+		groupsPromise = api.listGroups();
+	});
+
+	const createNewGroup = async () => {
+		const api = new GroupsApi(config);
+
+		const group = await api
+			.createGroup({ createGroup: { name: 'New group' } })
+			.catch(() => alerts.push('Failed to create new group', 'ERROR'));
+
+		if (!group) return;
+
+		const groups = await groupsPromise;
+
+		groupsPromise = new Promise(res => {
+			res([...groups, group]);
+		});
+	};
+</script>
+
+<div class="flex items-baseline justify-between p-4">
+	<h1 class="text-lg font-bold sm:text-xl">My Groups</h1>
+
+	<button onclick={createNewGroup} class="btn btn-primary btn-soft btn-sm sm:btn-md">
+		Create new
+	</button>
+</div>
+
+{#await groupsPromise}
+	<Loading />
+{:then groups}
+	<GroupList />
+{:catch}
+	<Error />
+{/await}
