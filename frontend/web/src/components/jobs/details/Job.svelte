fa6aa80feccd8dd2312f317e37db9f20a09a744b
tametsi <93092155+tametsi@users.noreply.github.com>
2026-03-13 19:29:31 +0100

    feat(ui): add job details

--- frontend/web/src/components/jobs/details/Job.svelte
+++ frontend/web/src/components/jobs/details/Job.svelte
@@ -1,24 +1,33 @@
 <script lang="ts">
-	import { config, JobsApi, type DisplayJobRun } from '@/api';
+	import { config, JobsApi, type DisplayJob, type DisplayJobRun } from '@/api';
 	import JobRuns from '@/components/jobs/details/JobRuns.svelte';
+	import Error from '@/components/shared/Error.svelte';
+	import Loading from '@/components/shared/Loading.svelte';
+	import { relativeTime } from '@/lib/formatter';
+	import { getJobName } from '@/lib/jobs';
 	import { ActivityIcon, PenIcon } from '@lucide/svelte';
 	import { onMount } from 'svelte';
 
-	let id = $state('');
+	let id = $state(''),
+		jobName = $state('');
 
-	let jobRunsPromise = $state(new Promise<DisplayJobRun[]>(() => {}));
+	let jobPromise = $state(new Promise<DisplayJob>(() => {})),
+		jobRunsPromise = $state(new Promise<DisplayJobRun[]>(() => {}));
 
 	onMount(async () => {
 		id = new URLSearchParams(window.location.search).get('id') ?? '';
 
 		const api = new JobsApi(config);
 
+		jobPromise = api.getJob({ id });
 		jobRunsPromise = api.listJobRuns({ id });
+
+		jobName = (await jobPromise).name;
 	});
 </script>
 
 <svelte:head>
-	<title>Job Name Details | guggr</title>
+	<title>Job {jobName} Details | guggr</title>
 </svelte:head>
 
 <div class="mb-4 flex items-baseline justify-between gap-2">
@@ -26,7 +35,7 @@
 		<menu>
 			<li><a href="/">Home</a></li>
 			<li><a href="/jobs">Jobs</a></li>
-			<li><a href="/jobs/details">Job Name</a></li>
+			<li><a href={`/jobs/details?id=${id}`}>Job Name</a></li>
 		</menu>
 	</div>
 
@@ -36,39 +45,58 @@
 </div>
 
 <div class="card card-side bg-base-100 shadow-md">
-	<figure class="text-primary/60 hidden p-6 md:block">
-		<ActivityIcon size="48" />
-	</figure>
+	{#await jobPromise}
+		<Loading />
+	{:then job}
+		<figure class="text-primary/60 hidden p-6 md:block">
+			<ActivityIcon size="48" />
+		</figure>
 
-	<div class="card-body flex flex-col justify-between gap-2 sm:flex-row sm:items-center">
-		<div>
-			<div class="card-title text-md text-base-content/90 sm:text-base-content sm:text-2xl">
-				<h2 class="truncate">
-					<span class="sr-only">Job name:</span>
-					Job name
-				</h2>
-				<span class="badge badge-primary badge-soft badge-sm whitespace-nowrap">
-					HTTP Job
-				</span>
+		<div class="card-body flex flex-col justify-between gap-2 sm:flex-row sm:items-center">
+			<div>
+				<div
+					class="card-title text-md text-base-content/90 sm:text-base-content sm:text-2xl"
+				>
+					<h2 class="truncate">
+						<span class="sr-only">Job name:</span>
+						{job.name}
+					</h2>
+					<span class="badge badge-primary badge-soft badge-sm whitespace-nowrap">
+						{getJobName(job.jobTypeId)} Job
+					</span>
+				</div>
+
+				<div class="text-base-content/80">
+					<span class="sr-only">Group: </span>
+					<!-- TODO group name -->
+					<a href="/groups" class="link link-hover">{job.groupId}</a>
+				</div>
 			</div>
 
-			<div class="text-base-content/80">
-				<span class="sr-only">Group: </span>
-				<a href="/groups" class="link link-hover">My cool group</a>
+			<div class="stats hidden sm:inline-grid">
+				{@render statusStat(job)}
 			</div>
 		</div>
-
-		<div class="stats hidden sm:inline-grid">
-			{@render statusStat()}
-		</div>
-	</div>
+	{:catch}
+		<Error />
+	{/await}
 </div>
 
 <div class="stats rounded-box bg-base-100 mt-4 w-full py-4 shadow-md sm:hidden">
-	{@render statusStat()}
+	{#await jobPromise}
+		<Loading />
+	{:then job}
+		{@render statusStat(job)}
+	{:catch}
+		<Error />
+	{/await}
 </div>
 
-{#snippet statusStat()}
+{#snippet statusStat(job: DisplayJob)}
+	{@const lastScheduledDiffMinutes = Math.round(
+		(Date.now() - (job.lastScheduled?.valueOf() || 0)) / 1000 / 60,
+	)}
+
 	<div class="stat py-0">
 		<div class="stat-title">Current Status</div>
 		<div class="stat-value text-success flex items-center gap-2 text-3xl">
@@ -80,7 +108,9 @@
 			</div>
 			Online
 		</div>
-		<div class="stat-desc">Last checked 2 minutes ago</div>
+		<div class="stat-desc">
+			Last checked {relativeTime.format(-lastScheduledDiffMinutes, 'minutes')}
+		</div>
 	</div>
 {/snippet}
 
